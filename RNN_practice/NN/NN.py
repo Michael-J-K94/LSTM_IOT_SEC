e0a0d067faf9fd9e706c455d07640c6261fc2493
Michael Jaemin Kim <michael.kim.604@gmail.com>
2019-03-15 17:28:14 +0900

    RNN/NN added

---
+++ RNN_practice/NN/NN.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# sigmoid function
+def nonlin(x,deriv=False):
+	if(deriv == True):
+		return x*(1-x)
+	return 1/(1+np.exp(-x))
+
+#input
+X = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1] ])
+#output
+y = np.array([[0,0,1,1]]).T
+print('X:\n',X)
+print('y:\n',y)
+
+#seed rand numbers to make calc
+np.random.seed(1)
+
+#initialize weights randomly with mean 0
+syn0 = 2*np.random.random((3,1))-1
+#syn1 = 2*np.random.random((4,1))-1
+print('syn0:\n',syn0)
+#print('syn1:\n',syn1)
+
+for j in range(10000):
+	#forward
+	l0 = X
+	l1 = nonlin(np.dot(l0,syn0))
+
+	#error
+	l1_error = y-l1
+
+	#multiply error with the slope of the sigmoid. at values in l1
+	l1_delta = l1_error*nonlin(l1,True)
+
+#update wiehgts
+	syn0 +=np.dot(l0.T,l1_delta)
+print('Output after train:\n',l1)
+
+
